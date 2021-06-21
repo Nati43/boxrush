5a2003f40f65898e4fa3ffd46b7ac5d8efb7fb5a
Natnael Meseret <natnael0meseret@gmail.com>
2021-06-21 14:12:21 +0300

    Minimalistic boxes game socket server.

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-const http = require('http');
+const server = require('http').createServer();
 
-const hostname = 'localhost';
 const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const activeRooms = [];
+
+io.of("/games").on("connection", (socket)=>{
+
+    socket.on('createRoom', (data)=>{
+        const roomID = data.cols+'-'+data.rows+'-'+socket.id
+        activeRooms.push(roomID);
+        socket.join(roomID);
+        socket.emit("roomCreated", roomID);
+    });
+    
+    socket.on("join", (rid)=>{
+        if(activeRooms.includes(rid)) {
+            socket.join(rid);
+            io.of('/games').in(rid).emit("joined");
+        }else{
+            socket.emit("joinError", "The link has expired.");
+        }
+    });
+
+    socket.on('markX', (marked)=>{
+        console.log("Marked: ", marked);
+        io.of('/games').in(marked.roomID).emit('markedX', marked);
+    });
+    
+    socket.on('markY', (marked)=>{
+        console.log("Marked: ", marked);
+        io.of('/games').in(marked.roomID).emit('markedY', marked);
+    });
+    
+    socket.on('win', (data)=>{
+        console.log("Winner: ", data);
+        io.of('/games').in(data.roomID).emit('winner', data);
+    });
+
+});
+
+server.listen(port, () => {
+  console.log(`Server running at: ${port}`);
 });
